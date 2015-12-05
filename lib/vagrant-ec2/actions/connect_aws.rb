d05da2fabe28f2f26928b83264f22224462b3fd0
Colin Hebert <hebert.colin@gmail.com>
2015-12-05 21:44:43 +1100

    Add support of credentials to the provider

--- lib/vagrant-ec2/actions/connect_aws.rb
+++ lib/vagrant-ec2/actions/connect_aws.rb
@@ -1,4 +1,4 @@
-require "aws-sdk"
+require 'aws-sdk'
 
 module VagrantPlugins
   module Ec2
@@ -10,10 +10,48 @@ module VagrantPlugins
 
         def call(env)
           Aws.use_bundled_cert!
+
           if env[:machine].provider_config.region
-            Aws.config.update({region: env[:machine].provider_config.region})
+            Aws.config[:region] = env[:machine].provider_config.region
           end
+          if env[:machine].provider_config.credentials
+            Aws.config[:credentials] = create_credentials(env[:machine].provider_config.credentials)
+          end
+
           @app.call(env)
+        end
+
+        def create_credentials(credentials)
+          obtained_credentials = nil
+          if credentials[:type] == :credentials
+            # Basic credentials
+            obtained_credentials = Aws::Credentials.new(
+              credentials[:access_key_id],
+              credentials[:secret_access_key],
+              credentials[:session_token]
+            )
+          elsif credentials[:type] == :shared
+            # Shared credentials from AWS-CLI
+            obtained_credentials = Aws::SharedCredentials.new(credentials[:options])
+          elsif credentials[:type] == :instance_profile
+            # Instance profile credentials
+            obtained_credentials = Aws::InstanceProfileCredentials.new(credentials[:options])
+          elsif credentials[:type] == :assume_role
+            # Assume role
+
+            # Obtain nested credentials to assume role
+            if credentials[:credentials]
+              credentials[:options][:credentials] = create_credentials(credentials[:credentials])
+            end
+
+            obtained_credentials = Aws::AssumeRoleCredentials.new(
+              client: Aws::STS::Client.new(credentials[:options]),
+              role_arn: credentials[:arn],
+              role_session_name: credentials[:session_name]
+            )
+          end
+
+          return obtained_credentials
         end
       end
     end
--- lib/vagrant-ec2/config.rb
+++ lib/vagrant-ec2/config.rb
@@ -11,20 +11,55 @@ module VagrantPlugins
       # @return [String]
       attr_accessor :region
 
+      # Credentials used to log in.
+      #
+      # @return [String]
+      attr_accessor :credentials
+
       def initialize
-        @ami    = UNSET_VALUE
-        @region = UNSET_VALUE
+        @ami         = UNSET_VALUE
+        @region      = UNSET_VALUE
+        @credentials = UNSET_VALUE
       end
 
       def finalize!
         @ami    = nil if @ami == UNSET_VALUE
         @region = nil if @region == UNSET_VALUE
+        @credentials = nil if @credentials == UNSET_VALUE
       end
 
       def validate(machine)
         errors = []
 
+        errors += validate_credentials(@credentials)
+
         { 'EC2 Provider' => errors }
+      end
+
+      def validate_credentials(credentials)
+        errors = []
+
+        if credentials == nil
+          # Nothing can make it fail here.
+        elsif ! credentials.is_a?(Hash)
+          errors << 'The credentials should be in a hash'
+        elsif credentials[:type] == :credentials
+          errors << 'An Access Key ID MUST be given' if !credentials[:access_key_id]
+          errors << 'An Access Key secret MUST be given' if !credentials[:secret_access_key]
+        elsif credentials[:type] == :shared
+          # Nothing can make it fail here.
+        elsif credentials[:type] == :instance_profile
+          # Nothing can make it fail here.
+        elsif credentials[:type] == :assume_role
+          errors << 'A role ARN to assume MUST be given' if !credentials[:arn]
+          credentials[:session_name] ||= 'vagrant-ec2_session'
+          credentials[:options] ||= Hash.new
+          errors += validate_credentials(credentials[:credentials])
+        else
+          errors << 'Invalid credential type {}'
+        end
+
+        return errors
       end
     end
   end
